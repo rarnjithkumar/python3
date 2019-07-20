6ed9564752e56686724a342cdbb7e4b3585a4959
logustarz <47684013+logustarz@users.noreply.github.com>
2019-07-20 15:01:02 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
 ll=input()
 if (ll=='a' or ll=='e' or ll=='i' or ll=='o'or ll=='u' or ll=='A' or ll=='E' or ll=='I' or ll=='O' or ll=='U'):
-    print("VOWEL")
+    print("Vowel")
 elif ((ll>="a" and ll<='z') or (ll>='A' and ll<='Z')):
     print("Consonant")
 else:
